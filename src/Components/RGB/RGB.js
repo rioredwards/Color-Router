4d1d0419910d12e38004642fee203b1ea25a6984
Rio Edwards <rioredwards@gmail.com>
2023-01-24 10:52:29 -0800

    Add RGB Route which grabs values from params

---
+++ src/Components/RGB/RGB.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import './RGB.css';
+
+export default function RGB() {
+  const { r, g, b } = useParams();
+  return <div style={{ backgroundColor: `rgb(${r},${g},${b})` }} className="rgb-container"></div>;
+}
